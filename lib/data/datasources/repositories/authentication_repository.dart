bf8b935a7efa63c3a778447ff58e96af7c99f2af
phamtanphat <phatdroid94@gmail.com>
2023-03-28 20:58:24 +0700

    repository

---
+++ lib/data/datasources/repositories/authentication_repository.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_app_sale_02022023/data/datasources/remote/api_request.dart';
+import 'package:flutter_app_sale_02022023/data/datasources/remote/app_response.dart';
+import 'package:flutter_app_sale_02022023/data/datasources/remote/dto/user_dto.dart';
+
+class AuthenticationRepository {
+  ApiRequest? _apiRequest;
+  
+  void setApiRequest(ApiRequest apiRequest) {
+    _apiRequest = apiRequest;
+  }
+  
+  Future signIn({required String email, required String password}) async{
+    Completer<UserDto> completerUserDto = Completer();
+    try {
+      Response response = await _apiRequest?.signInRequest(email: email, password: password);
+      AppResponse<UserDto> appResponse = AppResponse.fromJson(response.data, UserDto.fromJson);
+      completerUserDto.complete(appResponse.data);
+    } on DioError catch (e){
+      completerUserDto.completeError(e.response?.data["message"]);
+    } catch (e){
+      completerUserDto.completeError(e);
+    }
+    return completerUserDto.future;
+  }
+}
