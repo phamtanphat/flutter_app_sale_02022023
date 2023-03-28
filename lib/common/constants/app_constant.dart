e2971d7a9bce18aa919673dd632e8d9dfd3a58a1
phamtanphat <phatdroid94@gmail.com>
2023-03-28 20:44:32 +0700

    request

--- lib/common/constants/app_constant.dart
+++ lib/common/constants/app_constant.dart
@@ -1,3 +1,5 @@
 class AppConstant {
-  static final String BASE_URL = "https://serverappsale.herokuapp.com/";
+  static final String BASE_URL = "https://serverappsale.onrender.com/";
+
+  static final String SIGN_IN_URL = "user/sign-in";
 }
--- lib/data/datasources/remote/api_request.dart
+++ lib/data/datasources/remote/api_request.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_app_sale_02022023/common/constants/app_constant.dart';
 
 import 'dio_client.dart';
 
@@ -7,5 +8,12 @@ class ApiRequest {
   
   ApiRequest(){
     _dio = DioClient.instance.dio;
+  }
+
+  Future signInRequest({required String email, required String password}) {
+    return _dio.post(AppConstant.SIGN_IN_URL, data: {
+      "email" : email,
+      "password" : password
+    });
   }
 }
